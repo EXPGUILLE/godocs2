7016f2afe2f24bf6a19bef581fcfbee81a718f0d
EXPGUILLE <124185584+EXPGUILLE@users.noreply.github.com>
2023-04-10 14:52:40 -0400

    godocs

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('/godocs2', function () {
-    return view('godocs2');
+    return view('index');
 });
