83f89b32c40f2f9580d72e81af48df89856ff6ad
EXPGUILLE <124185584+EXPGUILLE@users.noreply.github.com>
2023-04-10 14:19:54 -0400

    godocs2

---
+++ resources/views/godocs2.blade.php
@@ -0,0 +1,140 @@
+<!doctype html>
+<html lang="zxx">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>GoDocs Documentation Theme</title>
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+        <link rel="stylesheet" href="style.css">
+    </head>
+        <body>
+            <section>
+                <header>
+                    <nav>
+                        <div class="container-header">
+                            <img src="https://demo.themefisher.com/godocs-bulma/images/logo.png" alt="logo">
+                            <ul class="lista1">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Inner Page</a></li>
+                                <li><a href="#">Search Page</a></li>
+                            </ul>
+                            
+                            <ul class="lista2">
+                                <li><button type="submit" class="boton1">Chanelog</button></li>
+                                <li><button type="submit" class="boton2">Contact</button></li>
+                            </ul>
+                        </div>
+                    </nav>   
+                </header> 
+            </section>
+            <section>
+                    <div class="container-documentarion">
+                            <div class="col1">
+                                <div class="doc">
+                                <h1>Documentation Theme By Themefisher Team</h1>
+                                <p>Lorem ipsum dolor amet, consetetur sadiffspscing elitr, diam nonumy invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua At.</p>
+                                </div>
+                                <input type="text" placeholder="Search here...">
+                                <button type="submit">Search</button>
+                            </div>
+                            <div class="col2">
+                                <img src="https://demo.themefisher.com/godocs-bulma/images/banner.jpg" alt="demo1">
+                            </div>    
+                        </div>
+            </section>
+            <section class="bcol">
+                
+            <div class="bcol1"><h1>Bcolumnsse Your Topics</h1></div>
+                <div class="grid-container2">
+                    <div class="bcol2">
+                        <h3>Basic Startup</h3>
+                        <p>Crast at dolor eget urna varius faucibus tempus in elit dolor sit amet.</p>
+                    </div>
+                    <div class="bcol2">
+                    <h3>Account Bill</h3>
+                        <p>Crast at dolor eget urna varius faucibus tempus in elit dolor sit amet.</p>
+                    </div>    
+                    <div class="bcol2">
+                    <h3>Our Features</h3>
+                        <p>Crast at dolor eget urna varius faucibus tempus in elit dolor sit amet.</p>
+                    </div>    
+                    <div class="bcol2">
+                    <h3>Theme Facility</h3>
+                        <p>Crast at dolor eget urna varius faucibus tempus in elit dolor sit amet.</p>
+                    </div>
+                </div>
+            </section>
+            <section>
+                <h3>Mostly Asked Questions</h3>
+                <div class="container">
+                    <div class="col">
+                        <div class="row">
+                            <h5>Will updates also be free?</h5>
+                            <p>Lorem, link ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto, mollitia delectus ut ab quibusdam. Magnam cumque numquam tempore reprehenderit illo, unde cum omnis vel sed temporibus. mollitia delectus ut ab quibusdam. Magnam cumque numquam tempore reprehenderit illo, unde cum omnis vel sed temporibus. mollitia delectus ut ab quibusdam. Magnam cumque numquam tempore reprehenderit illo, unde cum omnis vel sed temporibus.</p>
+                        </div>
+                        <div class="row">
+                            <h5>Is there any documentation and support?</h5>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto, mollitia delectus link ut ab quibusdam.</p>
+                        </div>    
+                        <div class="row">
+                            <h5>What is a product key?</h5>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt</p>
+                        </div>    
+                    </div>
+                    <div class="col">
+                        <div class="row">
+                            <h5>Discounts for students and Non Profit Organizations?</h5>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto.</p>
+                        </div>
+                        <div class="row">
+                            <h5>I need something unique, Can you make it?</h5>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto, mollitia delectus link ut ab quibusdam. Magnam cumque numquam tempore reprehenderit illo, unde cum omnis vel sed temporibus, repudiandae impedit nam ad enim porro, qui labore fugiat quod suscipit fuga necessitatibus. Perferendis, ipsum?</p>
+                        </div>    
+                        <div class="row">
+                            <h5>Any refunds?</h5>
+                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto.</p>
+                        </div>    
+                    </div>    
+                </div>
+            </section>
+            <section>      
+                <div class="container">  
+                    <div class="col">   
+                        <img src="https://demo.themefisher.com/godocs-bulma/images/cta-illustration.jpg" alt="demo2">                
+                    </div>
+                    <div class="col"> 
+                        <h2>Still Didn’t Find Your Answer?</h2>
+                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+                            <br>
+nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+                        <a href="#">Submit a Ticket</a>                  
+                    </div>
+                </div>  
+            </section>       
+        <footer>
+            <div class="container">
+                <div class="col">
+                    <ul>
+                        <li><a href="#">Changelog</a></li>
+                        <li><a href="#">Contact</a></li>
+                        <li><a href="#">Search</a></li>
+                    </ul>
+                </div>
+                <div class="col">
+                    <img src="https://demo.themefisher.com/godocs-bulma/images/logo.png" alt="logo">
+                </div>
+                <div class="col">
+                    <ul>
+                        <li><a href="#">Facebook</a></li>
+                        <li><a href="#">Twitter</a></li>
+                        <li><a href="#">GitHub</a></li>
+                        <li><a href="#">Linkedin</a></li>
+                    </ul>
+                </div> 
+                <div class="col">
+                    <p>Copyright © 2020 - Design & Developed by themefisher</p>
+                </div>    
+            </div>    
+        </footer>    
+        </body>
+</html>
