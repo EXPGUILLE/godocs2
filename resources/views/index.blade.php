f2023682d1709974e6e64c64f69c6e143215ca8e
EXPGUILLE <124185584+EXPGUILLE@users.noreply.github.com>
2023-04-14 10:26:30 -0400

    indexbt

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,137 +4,202 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>GoDocs Documentation Theme</title>
-        <link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
-        <link rel="stylesheet" href="style.css">
-    </head>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">    </head>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css">
+
         <body>
             <section>
                 <header>
-                    <nav>
-                        <div class="container-header">
-                            <img src="https://demo.themefisher.com/godocs-bulma/images/logo.png" alt="logo">
-                            <ul class="lista1">
-                                <li><a href="#">Home</a></li>
-                                <li><a href="#">Inner Page</a></li>
-                                <li><a href="#">Search Page</a></li>
-                            </ul>
-                            
-                            <ul class="lista2">
-                                <li><button type="submit" class="boton1">Chanelog</button></li>
-                                <li><button type="submit" class="boton2">Contact</button></li>
-                            </ul>
-                        </div>
-                    </nav>   
+                    <div class="p-4 mt-4 mb-4 mr-4 bg-dark text-white">
+                <nav class="navbar navbar-expand-lg bg-body-tertiary">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Inner Page</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">Search Page</a>
+        </li>
+
+
+      </ul>
+      <form class="d-flex" role="search">
+        <button class="btn btn-outline-success" type="submit">Changelog</button>
+        <button class="btn btn-outline-success" type="submit">Contact</button>
+
+      </form>
+    </div>
+  </div>
+</nav>
+<div>
                 </header> 
             </section>
             <section>
-                    <div class="container-documentarion">
-                            <div class="col1">
-                                <div class="doc">
-                                <h1>Documentation Theme By Themefisher Team</h1>
-                                <p>Lorem ipsum dolor amet, consetetur sadiffspscing elitr, diam nonumy invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua At.</p>
-                                </div>
-                                <input type="text" placeholder="Search here...">
-                                <button type="submit">Search</button>
-                            </div>
-                            <div class="col2">
-                                <img src="https://demo.themefisher.com/godocs-bulma/images/banner.jpg" alt="demo1">
-                            </div>    
+            <div class="container-documentarion mt-10">
+                <div class="row">
+                    <div class="col-md-6 order-md-1">
+                        <div class="doc">
+                            <h1 class="fw-bold">Documentation Theme<br> By Themefisher Team</h1>
+                            <p>Lorem ipsum dolor amet, consetetur sadiffspscing elitr, diam nonumy invidunt<br> ut labore et dolore magna aliquyam erat, sed diam voluptua At.</p>
                         </div>
+                            <form class="form-group">
+                                <div class="input-group">
+        <input class="form-control border-0 w-75" type="search" placeholder="Search here..." aria-label="Search">
+          <div class="input-group-append">
+          <div><button type="submit" class="btn btn-danger">Search</button></div>                  
+          </div>
+        </div>
+      </form>
+    </div>
+    <div class="col-md-6 order-md-2">
+      <img src="https://demo.themefisher.com/godocs-bulma/images/banner.jpg" alt="demo1">
+    </div>    
+  </div>
+</div>
+
             </section>
             <section class="bcol">
-                
-            <div class="bcol1"><h1>Bcolumnsse Your Topics</h1></div>
-                <div class="grid-container2">
-                    <div class="bcol2">
-                        <h3>Basic Startup</h3>
-                        <p>Crast at dolor eget urna varius faucibus tempus in elit dolor sit amet.</p>
-                    </div>
-                    <div class="bcol2">
-                    <h3>Account Bill</h3>
-                        <p>Crast at dolor eget urna varius faucibus tempus in elit dolor sit amet.</p>
-                    </div>    
-                    <div class="bcol2">
-                    <h3>Our Features</h3>
-                        <p>Crast at dolor eget urna varius faucibus tempus in elit dolor sit amet.</p>
-                    </div>    
-                    <div class="bcol2">
-                    <h3>Theme Facility</h3>
-                        <p>Crast at dolor eget urna varius faucibus tempus in elit dolor sit amet.</p>
-                    </div>
-                </div>
-            </section>
-            <section class="faq">
-                <h3>Mostly Asked Questions</h3>
-                <div class="container-will">
-                    <div class="column1">
-                        <div class="columna1">
-                            <h5>Will updates also be free?</h5>
-                            <p>Lorem, link ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto, mollitia delectus ut ab quibusdam. Magnam cumque numquam tempore reprehenderit illo, unde cum omnis vel sed temporibus. mollitia delectus ut ab quibusdam. Magnam cumque numquam tempore reprehenderit illo, unde cum omnis vel sed temporibus. mollitia delectus ut ab quibusdam. Magnam cumque numquam tempore reprehenderit illo, unde cum omnis vel sed temporibus.</p>
-                        </div>
-                        <div class="columna1">
-                            <h5>Is there any documentation and support?</h5>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto, mollitia delectus link ut ab quibusdam.</p>
-                        </div>    
-                        <div class="columna1">
-                            <h5>What is a product key?</h5>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt</p>
-                        </div>    
-                    </div>
-                    <div class="column2">
-                        <div class="columna2">
-                            <h5>Discounts for students and Non Profit Organizations?</h5>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto.</p>
-                        </div>
-                        <div class="columna2">
-                            <h5>I need something unique, Can you make it?</h5>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto, mollitia delectus link ut ab quibusdam. Magnam cumque numquam tempore reprehenderit illo, unde cum omnis vel sed temporibus, repudiandae impedit nam ad enim porro, qui labore fugiat quod suscipit fuga necessitatibus. Perferendis, ipsum?</p>
-                        </div>    
-                        <div class="columna2">
-                            <h5>Any refunds?</h5>
-                            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto.</p>
-                        </div>    
-                    </div>    
-                </div>
-            </section>
-            <section>      
-                <div class="still">  
-                    <div class="still-imagen">   
-                        <img src="https://demo.themefisher.com/godocs-bulma/images/cta-illustration.jpg" alt="demo2">                
-                    </div>
-                    <div class="col3"> 
-                        <h2>Still Didn’t Find Your Answer?</h2>
-                        <p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
-                            <br>
-nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
-                        <button type="submit">Submit a Ticket</button>                  
-                    </div>
-                </div>  
-            </section>       
-        <footer>
-            <div class="container-foot">
-                <div class="f1">
-                    <ul>
-                        <li><a href="#">Changelog</a></li>
-                        <li><a href="#">Contact</a></li>
-                        <li><a href="#">Search</a></li>
-                    </ul>
-                </div>
-                <div class="f2">
-                    <img src="https://demo.themefisher.com/godocs-bulma/images/logo.png" alt="logo">
-                </div>
-                <div class="f1">
-                    <ul>
-                        <li><a href="#">Facebook</a></li>
-                        <li><a href="#">Twitter</a></li>
-                        <li><a href="#">GitHub</a></li>
-                        <li><a href="#">Linkedin</a></li>
-                    </ul>
-                </div> 
+  <div class="bcol1">
+    <h1 class="fw-bold">Bcolumnsse Your Topics</h1>
+  </div>
+  <div class="grid-container2 d-flex justify-content-center">
+    <div class="bcol2 text-center m-5 shadow-lg rounded p-3">
+    <a href="#" class="btn btn-danger"><i class="bi bi-speedometer"></i></a>
+    <br><br>
+      <h3>Basic Startup</h3>
+      <p>Crast at dolor eget urna<br> varius faucibus tempus in<br> elit dolor sit amet.</p>
+    </div>
+    <div class="bcol2 text-center m-5 shadow-lg rounded p-3">
+    <a href="#" class="btn btn-danger"><i class="bi bi-credit-card"></i></a>
+    <br><br>
+      <h3>Account Bill</h3>
+      <p>Crast at dolor eget urna<br> varius faucibus tempus in<br> elit dolor sit amet.</p>
+    </div>
+    <div class="bcol2 text-center m-5 shadow-lg rounded p-3">
+    <a href="#" class="btn btn-danger"><i class="bi bi-box-seam"></i></a>
+    <br><br>
+      <h3>Our Features</h3>
+      <p>Crast at dolor eget urna<br> varius faucibus tempus in<br> elit dolor sit amet.</p>
+    </div>
+    <div class="bcol2 text-center m-5 shadow-lg rounded p-3">
+    <a href="#" class="btn btn-danger"><i class="bi bi-gear"></i></a>
+    <br><br>
+      <h3>Theme Facility</h3>
+      <p>Crast at dolor eget urna<br> varius faucibus tempus in<br> elit dolor sit amet.</p>
+    </div>
+  </div>
+</section>
+
+<section class="faq">
+    
+  <div class="ml-5 text-center">
+    <h2 class="fw-bold">Mostly Asked Questions</h2>
+</div>
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="faq-item m-3 shadow-lg rounded p-3">
+              <h5>Will updates also be free?</h5>
+              <p>Lorem, link ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto, mollitia delectus ut ab quibusdam. Magnam cumque numquam tempore reprehenderit illo, unde cum omnis vel sed temporibus. mollitia delectus ut ab quibusdam. Magnam cumque numquam tempore reprehenderit illo, unde cum omnis vel sed temporibus. mollitia delectus ut ab quibusdam.
+                Magnam cumque numquam tempore reprehenderit illo, unde cum omnis vel sed temporibus. </p>
             </div>
-                <div class="f4">
-                    <p>Copyright © 2020 - Design & Developed by themefisher</p>
-                </div>    
-        </footer>    
+          </div>
+          <div class="col-md-12">
+            <div class="faq-item m-3 shadow-lg rounded p-3">
+              <h5>Is there any documentation and support?</h5>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto, mollitia delectus link ut ab quibusdam. </p>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="faq-item m-3 shadow-lg rounded p-3">
+              <h5>What is a product key?</h5>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt</p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <div class="row">
+          <div class="col-md-12">
+            <div class="faq-item m-3 shadow-lg rounded p-3">
+              <h5>Discounts for students and Non Profit Organizations?</h5>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto. </p>
+            </div>
+          </div>
+          <div class="col-md-12">
+            <div class="faq-item m-3 shadow-lg rounded p-3">
+              <h5>I need something unique, Can you make it?</h5>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto, mollitia delectus link ut ab quibusdam. Magnam cumque numquam tempore reprehenderit illo, unde cum omnis vel sed temporibus, repudiandae impedit nam ad enim porro, qui labore fugiat quod suscipit fuga necessitatibus. Perferendis, ipsum?</p></div>
+          </div>
+          <div class="col-md-12">
+            <div class="faq-item m-3 shadow-lg rounded p-3">
+              <h5>Any refunds?</h5>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque praesentium nisi officiis maiores quia sapiente totam omnis vel sequi corporis ipsa incidunt reprehenderit recusandae maxime perspiciatis iste placeat architecto.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+<br><br>
+<section>      
+  <div class="container">  
+    <div class="row">
+      <div class="col-md-4">
+        <img src="https://demo.themefisher.com/godocs-bulma/images/cta-illustration.jpg" class="img-fluid" alt="demo2">                
+      </div>
+      <div class="col-md-8 d-flex flex-column justify-content-center"> 
+  <h2 class="mt-3 mt-md-0 fs-10 fw-bold">Still Didn’t Find Your Answer?</h2>
+  <p class="mb-3">Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam<br> nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam</p>
+  <div class="text-left"><button type="submit" class="btn btn-danger">Submit a Ticket</button></div>                  
+</div>
+    </div>
+  </div>
+</section>
+       <br>
+<footer>
+  <div class="container mt-5 mb-5">
+    <div class="row">
+      <div class="col-sm-4">
+        <ul class="list-inline mb-0">
+          <li class="list-inline-item"><a href="#" class="text-dark text-decoration-none">Changelog</a></li>
+          <li class="list-inline-item"><a href="#" class="text-dark text-decoration-none">Contact</a></li>
+          <li class="list-inline-item"><a href="#" class="text-dark text-decoration-none">Search</a></li>
+        </ul>
+      </div>
+      <div class="col-sm-4 text-center">
+        <img src="https://demo.themefisher.com/godocs-bulma/images/logo.png" alt="logo" class="img-fluid">
+      </div>
+      <div class="col-sm-4">
+        <ul class="list-inline mb-0 text-center">
+          <li class="list-inline-item"><a href="#" class="btn btn-danger"><i class="bi bi-facebook"></i></a></li>
+          <li class="list-inline-item"><a href="#" class="btn btn-danger"><i class="bi bi-twitter"></i></a></li>
+          <li class="list-inline-item"><a href="#" class="btn btn-danger"><i class="bi bi-github"></i></a></li>
+          <li class="list-inline-item"><a href="#" class="btn btn-danger"><i class="bi bi-linkedin"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <div class="container-fluid bg-light text-dark py-2">
+    <div class="row">
+      <div class="col-sm-12 text-center">
+        <p class="mb-0">© 2020 - Design & Developed by themefisher</p>
+      </div>
+    </div>
+  </div>
+</footer>
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
         </body>
 </html>
